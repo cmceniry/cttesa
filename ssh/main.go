b2261704d08471e0042d5fc5bd6c65f40d73b81c
Chris Mceniry <cmceniry@mit.edu>
2017-09-19 00:47:06 -0700

    * ssh exercise

---
+++ ssh/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"io"
+	"os"
+	"fmt"
+	"io/ioutil"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func getKey() (ssh.Signer) {
+	keyFile := "/home/vagrant/.ssh/id_rsa"
+	pemKey, err := ioutil.ReadFile(keyFile)
+	if err != nil {
+		panic(err)
+	}
+	signer, err := ssh.ParsePrivateKey(pemKey)
+	if err != nil {
+		panic(err)
+	}
+	return signer
+}
+
+func connect(host string, signer ssh.Signer) *ssh.Client {
+	config := &ssh.ClientConfig{
+		User: "vagrant",
+		Auth: []ssh.AuthMethod{ssh.PublicKeys(signer)},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	client, err := ssh.Dial("tcp", "127.0.0.1:22", config)
+	if err != nil {
+		panic(err)
+	}
+	return client
+}
+
+func main() {
+	signer := getKey()
+	client := connect("127.0.0.1:22", signer)
+
+	session, err := client.NewSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	sin, err := session.StdinPipe()
+	if err != nil {
+		panic(err)
+	}
+	err = session.Start(...)
+	if err != nil {
+		panic(err)
+	}
+	...
+	session.Wait()
+}
